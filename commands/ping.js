e504e08b08594c0b0ecb6606e7537177bd232662
Hayden Brown <nedyah.brown@gmail.com>
2020-02-04 07:16:41 +0000

    🐛 Fixed heartbeat ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,7 @@
 module.exports = async (client, message, args) => {
     let time = new Date();
     message.channel.send({embed: client.util.embed(message, '🕗 Pinging...', 'warn')}).then(async ctx => {
-        setTimeout(() => {
-            ctx.edit({embed: client.util.embed(message, `🏓 Pong! This message took me ${Math.floor(client.ws.ping)}ms to send!`, undefined, `Heartbeat: ${Math.floor(Date.now() - message.createdTimestamp)}ms`)})
-        }, Math.floor(client.ws.ping));
+            ctx.edit({embed: client.util.embed(message, `🏓 Pong! This message took me ${Math.floor(client.ws.ping)}ms to send!`, undefined, `Heartbeat: ${Math.floor(ctx.createdTimestamp - message.createdTimestamp)}ms`)})
     })
 };
 
